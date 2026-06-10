098730da705816c7a5b66b2434ca386171f2a89c
ElreversoMain <felipe123oliveirasantos@gmail.com>
2026-06-10 11:41:09 -0300

    ExercicioLogica

--- ExerciciosLogica/index.js
+++ ExerciciosLogica/index.js
@@ -6,3 +6,14 @@ console.log(resultado)
 const retirar=prompt("qual tarefa deseja retirar")
 resultado.splice(retirar,1)
 console.log(resultado)
+
+const DigiteNome=prompt("Digite seu nome")
+const DigiteEmail=prompt("Digite seu email")
+console.log(`Seja bem vindo ${DigiteNome}, seu email ${DigiteEmail} foi cadastrado com sucesso`)
+
+const ComidaFavorita=["arroz","feijao","batata","macarrao","soja"]
+console.log(ComidaFavorita)
+const Adicionar=prompt("digite sua outra comida favorita")
+ComidaFavorita.splice(1,1,Adicionar)
+console.log(ComidaFavorita)
+
